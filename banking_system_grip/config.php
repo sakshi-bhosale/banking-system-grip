df4c2ebad8e9c69f570886f960037bbee1a7554f
sakshi-bhosale <84279181+sakshi-bhosale@users.noreply.github.com>
2021-05-21 15:35:20 +0530

    Add files via upload

---
+++ banking_system_grip/config.php
@@ -0,0 +1,12 @@
+<?php
+	$servername = "localhost";
+	$user = "root";
+	$pass = "";
+	$dbname ="bankdata";
+	$conn = mysqli_connect($servername,$user,$pass,$dbname);
+
+	if(!$conn){
+		die("Could Not Connect to the database".mysqli_connect_error());
+	}
+
+?>
